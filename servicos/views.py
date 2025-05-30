77448e0f096a40479f2e43c7ecfe560e079da380
analiciasouza <analiciasouza.11@gmail.com>
2025-05-30 17:18:10 -0300

    Criação e Estilização da página listando serviços

--- servicos/views.py
+++ servicos/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from home.models.servicos import Servico
 
+
+def list_servicos(request):
+    servicos = Servico.objects.all()
+    context = {
+        'servicos' : servicos
+    }
+    return render(request, 'servicos/secao-servicos.html', context )
+
+
 def detail_servico(request, servico_id):
     servico = Servico.objects.get(pk=servico_id)
     context = {
